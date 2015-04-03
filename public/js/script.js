8f472d162c5d3c8ff15f3236a45587c96291faf5
juan david cante <juanvc123@hotmail.com>
2015-04-03 12:31:06 -0500

    agrengando angular route, list angular menus y templates

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,34 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
+var estatutoApp = angular.module('StarterApp', ['ngMaterial', 'ngRoute']);
 
-app.config(['$mdThemingProvider', '$mdIconProvider',
-	function($mdThemingProvider,$mdIconProvider) {
-		$mdThemingProvider.theme('default')
-			.primaryPalette('teal')
-			.accentPalette('blue')
+estatutoApp.config(['$mdThemingProvider', '$mdIconProvider','$routeProvider',
+	function($mdThemingProvider,$mdIconProvider,$routeProvider) {
 
-        $mdIconProvider
-            .icon('facebook', 'images/svg/facebook.svg', 24)
-            .icon('twitter', 'images/svg/twitter.svg', 24)
-            .icon('about', 'images/svg/about.svg', 24)
-            .icon('help', 'images/svg/help.svg')
-	}
+		$mdThemingProvider.
+      theme('default')
+    		.primaryPalette('red')
+    		.accentPalette('blue')
+
+    $mdIconProvider
+      .icon('facebook', 'images/svg/facebook.svg', 24)
+      .icon('twitter', 'images/svg/twitter.svg', 24)
+      .icon('about', 'images/svg/about.svg', 24)
+      .icon('help', 'images/svg/help.svg')
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/index/index.html',
+        controller: 'indexController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+    }
 ])
-app.controller('AppCtrl', function($scope, $mdSidenav,$timeout,$mdBottomSheet,$mdDialog){
+estatutoApp.controller('indexController', function(){
+
+})
+
+estatutoApp.controller('AppCtrl', function($scope, $mdSidenav,$timeout,$mdBottomSheet,$mdDialog){
     //toggleSidenav
     $scope.toggleSidenav = function(menuId) {
 
@@ -50,74 +65,18 @@ app.controller('AppCtrl', function($scope, $mdSidenav,$timeout,$mdBottomSheet,$m
             $scope.alert = 'You cancelled the dialog.';
         });
     };
+
+    $scope.menus = [
+      {name: 'inicio', url:'index'},
+      {name: 'Explorador del estatuto', url:'explorador-del-estatuto'},
+      {name: 'Reformas tributarias', url:'reformas-tributarias'},
+      {name: 'Vencimientos', url:'vencimientos-impuestos'},
+    ]
+    
 });
 
 
-(function () {
-  'use strict';
-  app.controller('DemoCtrl', DemoCtrl);
-  function DemoCtrl ($timeout, $q, $log) {
-    var self = this;
-    // list of `state` value/display objects
-    self.states        = loadAll();
-    self.selectedItem  = null;
-    self.searchText    = null;
-    self.querySearch   = querySearch;
-    self.simulateQuery = false;
-    self.isDisabled    = false;
-    self.selectedItemChange = selectedItemChange;
-    // ******************************
-    // Internal methods
-    // ******************************
-    /**
-     * Search for states... use $timeout to simulate
-     * remote dataservice call.
-     */
-    function querySearch (query) {
-      var results = query ? self.states.filter( createFilterFor(query) ) : [],
-          deferred;
-      if (self.simulateQuery) {
-        deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
-      } else {
-        return results;
-      }
-    }
-    function selectedItemChange(item) {
-      $log.info('Item changed to ' + item);
-    }
-    /**
-     * Build `states` list of key/value pairs
-     */
-    function loadAll() {
-      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-      return allStates.split(/, +/g).map( function (state) {
-        return {
-          value: state.toLowerCase(),
-          display: state
-        };
-      });
-    }
-    /**
-     * Create filter function for a query string
-     */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-      return function filterFn(state) {
-        return (state.value.indexOf(lowercaseQuery) === 0);
-      };
-    }
-  }
-})();
-
-app.controller('GridBottomSheetCtrl', function($scope, $mdBottomSheet) {
+estatutoApp.controller('GridBottomSheetCtrl', function($scope, $mdBottomSheet) {
     $scope.items = [     
         { name: 'Ayuda', icon: 'help' },        
         { name: 'Twitter', icon: 'twitter', url: 'https://www.google.com' },      
