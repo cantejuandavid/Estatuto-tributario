1f439f2d2401051dfedeaea36ac922b17ed32f89
juan david cante <juanvc123@hotmail.com>
2015-06-02 17:48:55 -0500

    add mognoose data

---
+++ routes/mongoose.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose')
+var db = 'mongodb://user_2015_estatuto:juandavid123@ds031952.mongolab.com:31952/estatuto_tributario_colombia'
+var db = module.exports = mongoose.connect(db)
+
+
+mongoose.connection.on('error', function(err) {
+	console.log(err)
+})
+
+mongoose.connection.on('connected', function(e) {
+	console.log('->DB Lista!')	
+})
