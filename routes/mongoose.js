be6aced8cb563b0aa1e85c827260e31a4cf76617
juan david <juanvc123@hotmail.com>
2016-10-03 14:16:56 -0500

    Dejando datos de la DB en un .json

---
+++ routes/mongoose.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+
+var data = require("./DBData.json")
+
+var db = 'mongodb://'+data.user+':'+data.pass+'@'+data.instance+'.mongolab.com:'+data.port+'/estatuto_tributario_colombia'
+mongoose.Promise = global.Promise;
+var db = module.exports = mongoose.connect(db)
+
+
+mongoose.connection.on('error', function(err) {
+	console.log(err)
+})
+
+mongoose.connection.on('connected', function(e) {
+	console.log('->DB Lista!!!')
+})
