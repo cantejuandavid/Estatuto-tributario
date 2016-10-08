856728712578c0f7dc97250d7c20fb476749cad3
juan david <juanvc123@hotmail.com>
2016-10-08 16:25:08 -0500

    gitignore chan

--- routes/mongoose.js
+++ routes/mongoose.js
@@ -8,7 +8,7 @@ var db = module.exports = mongoose.connect(db)
 
 
 mongoose.connection.on('error', function(err) {
-	console.log(err)
+	console.log(err);
 })
 
 mongoose.connection.on('connected', function(e) {
